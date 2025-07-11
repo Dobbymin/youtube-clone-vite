80353e885f0f6af50a7b0cb1d922d9bbe7573184
Dobbymin <gmin990624@gmail.com>
2025-07-12 01:21:09 +0900

    init: 전체 구조 생성 및 개발 환경 세팅

---
+++ eslint.config.js
@@ -0,0 +1,78 @@
+import js from '@eslint/js';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import prettier from 'eslint-plugin-prettier';
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import globals from 'globals';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{js,jsx}'],
+    ignores: ['dist', '**/*.config.js', '**/*.config.mjs'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      prettierConfig,
+      importPlugin.flatConfigs.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      react: react,
+      prettier: prettier,
+    },
+    rules: {
+      ...react.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'prettier/prettier': ['warn', { usePrettierrc: false }],
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-uses-react': 'off',
+      'import/order': [
+        'off',
+        {
+          'newlines-between': 'always', // import 사이에 한 줄 띄우기
+          alphabetize: { order: 'asc', caseInsensitive: true },
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling'],
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '@/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['builtin'],
+        },
+      ],
+    },
+    settings: {
+      react: {
+        version: '19.1.0',
+      },
+    },
+  },
+]);
