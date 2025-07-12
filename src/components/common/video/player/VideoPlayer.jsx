a114c7bf17a2f9aa778b9d632619f0d6c8722966
Dobbymin <gmin990624@gmail.com>
2025-07-12 17:56:36 +0900

    fix: video 경로 변경

--- src/components/common/video/player/VideoPlayer.jsx
+++ src/components/common/video/player/VideoPlayer.jsx
@@ -1,11 +1,13 @@
-import video from '@/video/올해부턴 CSS 다르게 짬 ㅅㄱ (2022년 CSS 채신기술).mp4';
-
 import styles from './video-player.module.css';
 
 export const VideoPlayer = () => {
   return (
     <section className={styles.player}>
-      <video controls className={styles.video} src={video}></video>
+      <video
+        controls
+        className={styles.video}
+        src='/video/demo-video.mp4'
+      ></video>
     </section>
   );
 };
